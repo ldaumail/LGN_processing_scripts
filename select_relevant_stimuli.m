e8a30a9ca6f84d185c926e6c8cf85f47afb1e38d
ldaumail <49167117+ldaumail@users.noreply.github.com>
2019-10-08 09:15:48 -0500

    Add files via upload

--- select_relevant_stimuli.m
+++ select_relevant_stimuli.m
@@ -16,17 +16,17 @@ end
 npmkdir    = 'C:\Users\maier\Documents\MATLAB\NPMK-master\'; 
 nbanadir   = 'C:\Users\maier\Documents\bootcamp-selected\nbanalysis\'; 
 
-directory  = 'C:\Users\maier\Documents\LGN_data\LGN_190326_B-cinterocdrft001_data\';
-BRdatafile = '190326_B_cinterocdrft001';
+directory  = 'C:\Users\maier\Documents\LGN_data\190119_B_cinterocdrft002_data\';
+BRdatafile = '190119_B_cinterocdrft002';
 filename   = [directory BRdatafile]; 
 
 addpath(genpath(directory))
 addpath(genpath(npmkdir))
 addpath(genpath(nbanadir))
 
-aMUA = load(strcat(filename, 'aMUA.mat'));  
-LFP = load(strcat(filename, 'LFP.mat')); 
-CSD = load(strcat(filename, 'CSD.mat'));
+aMUA = load(strcat(filename, '_aMUA_hp15.mat'));  
+LFP = load(strcat(filename, '_LFP_hp15.mat')); 
+CSD = load(strcat(filename, '_CSD_hp15.mat'));
 
 %% STEP ONE: LOAD STIMULUS CONDITIONS with text file 
 
@@ -79,24 +79,80 @@ EventTimes      = floor(NEV.Data.SerialDigitalIO.TimeStampSec.*1000); % convert 
 
 STIM            = sortStimandTimeData(grating,pEvC,pEvT,'stim'); % this is in nbanalysis. definitely double check it before you use it. 
 
-tridx = STIM.contrast >=0.5 & STIM.fixedc == 0;
-%tridx = STIM.contrast >= 0.5 & STIM.fixedc >= 0.5;
-contrast = '_domsup50_nondom0';
+ %{
+data_header = struct();
 
-%if you already have the data stored into a file, write it like
+        eval(['data_header.domsup50' ...
+            '_nondom0='...
+            'STIM.contrast >= 0.5'...
+            ' & STIM.fixedc == 0'])
+      
+       eval(['data_header.dom0' ...
+            '_nondomsup50='...
+            'STIM.contrast == 0'...
+            ' & STIM.fixedc >= 0.5'])
+        
+       eval(['data_header.domsup50' ...
+            '_nondomsup50='...
+            'STIM.contrast >= 0.5'...
+            ' & STIM.fixedc >= 0.5'])   
+%}
+
+dom = unique(STIM.contrast);
+nondom = unique(STIM.fixedc);
+for temp_c = dom
+    for temp_f = nondom
+disp(temp_c)
+disp(temp_f)
+end
+end
+for temp_c = dom'
+    for temp_f = nondom'
+        
+
+        eval(['data_header.domsup' num2str(temp_c*1000) ...
+            'nondom' num2str(temp_f*1000) '='...
+            'STIM.contrast == ' num2str(temp_c)...
+            ' & STIM.fixedc ==' num2str(temp_f)])
+                
+    end
+end
+  %}
+ %WARNING: the following logical vectors are obtained from 'grating' object,
+ %itself obtain from the textfile, that has all the event codes of the
+ %whole session. However, the '.ns6' file is of different length, as only
+ %a certain number of trials of the session are include in -s00x.ns6' files
+ % ==> don't forget to adjust the size accordingly when calling the
+ % contrast.
+data_header.contrast1 = STIM.contrast ==0 & STIM.fixedc >= 0.5;
+data_header.contrast2 = STIM.contrast >=0.5 & STIM.fixedc == 0;
+data_header.contrast3 = STIM.contrast >=0.5 & STIM.fixedc >= 0.5;
+
+f = {'_dom0_nondomsup50','_domsup50_nondom0','_domsup50_domsup50'};
+
+%f.data_header= {'_dom0_nondomsup50','_domsup50_nondom0','_domsup50_nondomsup50'};
+%[data_header.(f)] = f{'_dom0_nondomsup50','_domsup50_nondom0','_domsup50_nondomsup50'};
+
+%contrast = '_domsup50_nondomsup50';
+
+%% if you already have the data stored into a file, write it like
 %this:
+cont_aMUA = aMUA.aMUA_data(1:1551,:,data_header.contrast1(1:length(aMUA.aMUA_data(1,1,:))));
+cont_LFP = LFP.LFP_data(1:1551,:,data_header.contrast1(1:length(LFP.LFP_data(1,1,:))));
+cont_CSD = CSD.CSD_data(1:1551,:,data_header.contrast1(1:length(CSD.CSD_data(1,1,:))));
 
-cont_aMUA = aMUA.STIM_aMUA(1:1551,:,tridx);  
-cont_LFP = LFP.STIM_LFP(1:1551,:,tridx); 
-cont_CSD = CSD.STIM_CSD(1:1551,:,tridx); 
-
+%{
+cont_aMUA = aMUA.STIM_aMUA(1:1551,:,data_header.contrast1(1:length(aMUA.STIM_aMUA(1,1,:))));
+cont_LFP = LFP.STIM_LFP(1:1551,:,data_header.contrast1(1:length(LFP.STIM_LFP(1,1,:))));
+cont_CSD = CSD.STIM_CSD(1:1551,:,data_header.contrast1(1:length(CSD.STIM_CSD(1,1,:))));
+%}
 
 %% Create plots
 %% plot baseline corrected mean normalized data
 
 data = cat(4,cont_LFP, cont_aMUA, cont_CSD);
-newLimePlot(data, BRdatafile, filename, contrast)
- 
+%newLimePlotExclusive(data, BRdatafile, filename, f{3})
+newLimePlot(data, BRdatafile, filename, f{1})
  
    %LFP
  LimePlot(cont_LFP, BRdatafile, filename, contrast)
@@ -108,8 +164,26 @@ newLimePlot(data, BRdatafile, filename, contrast)
  SurfacePlot(cont_LFP, BRdatafile, filename, contrast)
  SurfacePlot(cont_aMUA, BRdatafile, filename, contrast)
  SurfacePlot(cont_CSD, BRdatafile, filename, contrast)
- 
- 
+
+ %% stimuli duration time
+onsettimes =EventTimes(EventCodes ==24);
+offsettimes = EventTimes(EventCodes ==23);
+correctev = EventTimes(EventCodes ==96);
+
+selected_onsettimes= nan(1,length(onsettimes));
+selected_offsettimes=nan(1,length(offsettimes));
+for n = 1:length(correctev) 
+  selected_onsettimes(n)  = EventTimes(find(EventCodes(1:length(correctev)) == 23,1,'last'));
+  selected_offsettimes(n) = EventTimes(find(EventCodes(1:length(correctev)) == 24,1,'last'));
+end
+    
+     
+        
+stimtime1 = selected_onsettimes(1)- selected_offsettimes(1);
+stimtime2 = selected_onsettimes(2)- selected_offsettimes(2);
+stimtime3 = selected_onsettimes(3)- selected_offsettimes(3);
+
+%%
  %{
  mean_LFP = mean(cont_LFP,3);
  norm_mean = nan(length(mean_data(:,1)),length(mean_data(1,:)));
